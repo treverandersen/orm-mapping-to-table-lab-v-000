09595d713e85db6eea1dfa8f679cb080a01e6ff2
Trever Andersen <trever.r.andersen@gmail.com>
2017-11-10 18:16:00 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,5 +11,15 @@ class Student
     @grade = grade
   end
 
+  def self.create_table
+    sql = <<-SQL 
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+      )
+      SQL 
+    DB[:conn].execute(sql)
+  end
 
 end
