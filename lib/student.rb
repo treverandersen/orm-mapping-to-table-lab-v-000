6b2453d514c46d8df18a4b60b2d441ac1016eb62
Trever Andersen <trever.r.andersen@gmail.com>
2017-11-10 18:22:36 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,4 +22,12 @@ class Student
     DB[:conn].execute(sql)
   end
 
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE students
+      )
+      SQL
+    DB[:conn].execute(sql)
+  end
+
 end
