fd6cfeb65d101daed8e32466e1f954adb0af3dd8
Trever Andersen <trever.r.andersen@gmail.com>
2017-11-10 18:29:33 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,15 +18,23 @@ class Student
         name TEXT,
         grade TEXT
       )
-      SQL
+    SQL
     DB[:conn].execute(sql)
   end
 
   def self.drop_table
     sql = <<-SQL
       DROP TABLE students
-      SQL
+    SQL
     DB[:conn].execute(sql)
+  end
+
+  def save 
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade)
   end
 
 end
