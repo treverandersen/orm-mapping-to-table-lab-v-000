85aa42f39ec61e72268ef776ac8188efff2a726a
Trever Andersen <trever.r.andersen@gmail.com>
2017-11-10 18:38:19 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,4 +38,10 @@ class Student
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
+
 end
